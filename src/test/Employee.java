ca4ed9e443dc150a401f1c0a2cfa53e6830eff12
srvkmr699@gmail.com <srvkmr699@gmail.com>
2019-09-27 15:01:48 +0530

    added getter & setters

--- src/test/Employee.java
+++ src/test/Employee.java
@@ -6,4 +6,23 @@ public class Employee {
 	private String employeeName;
 	private double employeeSalary;
 	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+	
 }
