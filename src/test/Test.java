819abbffe10e70f04c00ea41ee3365326af17c2b
srvkmr699@gmail.com <srvkmr699@gmail.com>
2019-09-27 13:00:23 +0530

    display method added

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,5 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is main method");
 	}
+	public void display() {
+		System.out.println("this is dummy method");
+	}
 
 }
